9396b2a9401a6ec066ae07c5625d5e0375b5b7a9
krishna upadhyay <kupadhyay2394@gmail.com>
2025-09-10 21:42:14 +0530

    added certification

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -11,7 +11,7 @@ const Certifications = () => {
           <p>Issued: Aug 2025</p>
         </div>
         <div className="cert-actions">
-          <a className="btn btn-outline btn-small" href="https://www.credly.com/" target="_blank" rel="noreferrer">View Certificate</a>
+          <a className="btn btn-outline btn-small" href="https://catalog-education.oracle.com/ords/certview/sharebadge?id=BDE55157E4D980AEF4F0B06BB628363B0AEE97C62950C8ABC50871CB2ACC447A" target="_blank" rel="noreferrer">View Certificate</a>
         </div>
       </div>
       <div className="card cert-item">
@@ -23,8 +23,22 @@ const Certifications = () => {
           <p>Issued: Jul 2024</p>
         </div>
         <div className="cert-actions">
-          <a className="btn btn-outline btn-small" href="https://www.hackerrank.com/certificates" target="_blank" rel="noreferrer">View Certificate</a>
+          <a className="btn btn-outline btn-small" href="https://www.hackerrank.com/certificates/2921d81b9a34" target="_blank" rel="noreferrer">View Certificate</a>
         </div>
+        
+      </div>
+      <div className="card cert-item">
+        <div className="cert-icon" aria-hidden="true">
+        <img src="/images/hacker.png" alt="Oracle" className="cert-logo" />
+        </div>
+        <div className="cert-content">
+          <h3>HackerRank JavaScript (Intermediate) Certificate</h3>
+          <p>Issued: Sep 2024</p>
+        </div>
+        <div className="cert-actions">
+          <a className="btn btn-outline btn-small" href="https://www.hackerrank.com/certificates/f0cdf597634e" target="_blank" rel="noreferrer">View Certificate</a>
+        </div>
+        
       </div>
     </div>
   )
